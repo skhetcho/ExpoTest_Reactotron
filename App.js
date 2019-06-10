48eed645cd3b191c680723f04a8620aed70dd181
Souren <khetcho@gmail.com>
2019-06-10 04:36:54 -0700

    Reactotron integration to an emptry expo (SDK 33) project

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Reactotron, {networking} from 'reactotron-react-native'
+
+
+Reactotron
+  .configure({host: '192.168.0.21'}) //this is also the ip address that expo is using
+  .use(networking({
+    ignoreUrls: /\/(logs|symbolicate)$/,
+  }))
+  .useReactNative()
+  .connect()
+
+  export default class App extends React.Component {
+    render() {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.paragraph}>
+            Hello Reactotron; Some Text to test
+          </Text>
+        </View>
+      );
+    }
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#ecf0f1',
+      padding: 8,
+    },
+    paragraph: {
+      margin: 24,
+      fontSize: 18,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+  });
+  
